8ee2a1768f9392813ed1750fe159bb0c8dea499c
Wilson <Fishas02@gmail.com>
2024-07-16 16:25:37 -0500

    agregado de estilos en menu-list-item junto con su activacion

--- Ultimo-Proyecto-Idat-Eccomerce/src/pages/Home/Home.jsx
+++ Ultimo-Proyecto-Idat-Eccomerce/src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
-export const Home = () => {
+const Home = () => {
+
+  const [categary,setCategory] = useState("All")
+
   return (
     <div>
       <Header/>
+      <ExploreMenu categary={categary} setCategory={setCategory}/>
     </div>
   )
 }
